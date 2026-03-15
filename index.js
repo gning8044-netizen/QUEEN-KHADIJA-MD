8e216ab9bfc210bdc49ec0a49d7ff670ca109f5c
Shadow prime <gning8044@gmail.com>
2026-03-15 11:20:41 +0000

    Create index.js

---
+++ index.js
@@ -0,0 +1,7 @@
+import connectToWhatsapp from './Khadija/shadow.js'
+import handleIncomingMessage from './events/messageHandler.js'
+
+(async() => {
+    await connectToWhatsapp(handleIncomingMessage)
+        console.log('established !')
+})()
